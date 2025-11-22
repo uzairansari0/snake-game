0853688394d1a53026e66d29d32c8a77d695b586
Monika Surywanshi <01uzairansari@gmail.com>
2025-11-22 18:21:00 +0530

    addedlogic

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const board = document.querySelector(".board");
 const blockHeight = 30;
 const blockWidth = 30;
 
+let intervalId = null;
+
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let food = {
+  x: Math.floor(Math.random() * rows),
+  y: Math.floor(Math.random() * cols),
+};
 
 // for (let i = 0; i < rows * cols; i++) {
 //   const block = document.createElement("div");
@@ -17,15 +23,8 @@ const snake = [
     x: 1,
     y: 3,
   },
-  {
-    x: 1,
-    y: 4,
-  },
-  {
-    x: 1,
-    y: 5,
-  },
 ];
+let direction = "right";
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
@@ -37,3 +36,50 @@ for (let row = 0; row < rows; row++) {
     blocks[`${row}-${col}`] = block;
   }
 }
+
+function render() {
+  let head = null;
+
+  blocks[`${food.x}-${food.y}`].classList.add("food");
+
+  if (direction === "left") head = { x: snake[0].x, y: snake[0].y - 1 };
+  else if (direction === "right") head = { x: snake[0].x, y: snake[0].y + 1 };
+  else if (direction === "down") head = { x: snake[0].x + 1, y: snake[0].y };
+  else if (direction === "up") head = { x: snake[0].x - 1, y: snake[0].y };
+
+  if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+    alert("Game Over");
+    clearInterval(intervalId);
+  }
+
+  if (head.x === food.x && head.y === food.y) {
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+    food = {
+      x: Math.floor(Math.random(rows)),
+      y: Math.floor(Math.random(cols)),
+    };
+    blocks[`${food.x}-${food.y}`].classList.add("food");
+  }
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+  });
+
+  snake.unshift(head);
+  snake.pop();
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+  });
+}
+
+intervalId = setInterval(() => {
+  render();
+}, 400);
+
+addEventListener("keydown", (e) => {
+  if (e.key == "ArrowUp") direction = "up";
+  else if (e.key == "ArrowDown") direction = "down";
+  else if (e.key == "ArrowRight") direction = "right";
+  else if (e.key == "ArrowLeft") direction = "left";
+});
